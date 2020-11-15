b4e3f98156279442c33d837da62f229706f37747
Kayla Newlon <kbnewlon@hotmail.com>
2020-11-14 18:01:42 -0800

    added create adventure rating route

---
+++ routes/createAdventureRating.js
@@ -0,0 +1,21 @@
+const db = ('../models');
+
+//create adventure rating route
+app.put('./Adventure_rating/:id', function (req,res){
+    db.Adventure_rating.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(function (data) {
+        if (!data) {
+            res.status(404).json(data)
+        } else {
+            data.addRating(req.params.Adventure_rating_id)
+            res.status(200).json(data)
+        }
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+});
+
+module.exports = app;
