db1aada6027887e9ae5fe2ae6ed234ff3b5eee6b
Kayla Newlon <kbnewlon@hotmail.com>
2020-11-14 18:04:48 -0800

    delete adventure rating route

---
+++ routes/deleteAdventureRating.js
@@ -0,0 +1,19 @@
+const db = ('../models');
+
+//delete adventure rating route
+app.delete('./Adventure_rating/:id', function (req,res){
+    db.Adventure_rating.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(data => {
+        console.log(data)
+        res.status(200).json(data)
+
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = app;
