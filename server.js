22e2fe6b330a8d931c52c964e64789b8c6f673dc
Andrew Crow <andrew@crow.me>
2020-11-16 00:30:06 -0800

    added create tag to server.js

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use(
 require("./routes/createCompanyUser")(app);
 require("./routes/createAdventureCompany")(app);
 require("./routes/createAdventure")(app);
+require("./routes/createTag")(app);
 require("./routes/loginUser")(app);
 require("./routes/registerUser")(app);
 require("./routes/forgotPassword")(app);
