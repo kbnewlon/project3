937245973417c86a01a9fb0c64cb6494ea1f49f5
Andrew Crow <andrew@crow.me>
2020-11-16 00:23:23 -0800

    added fix for adventure route

--- routes/createAdventure.js
+++ routes/createAdventure.js
@@ -1,20 +1,22 @@
 const db = require("../models");
 
 //create new adventure
-app.post('/adventure', function (req, res) {
-    db.Adventure.create({ 
-        name: req.body.name,
-        description: req.body.description,
-        image: req.body.image,
-        longitude: req.body.longitude,
-        latitude: req.body.latitude,
-        Adventure_companyId: req.body.Adventure_companyId
-    }).then(function (data) {
-        console.log(data);
-        res.status(200).json(data);
-    }).catch(err => {
-        res.status(500).json(err)
-    })
-});
-
-module.exports = app;
+module.exports = (app) => {
+    app.post("/adventure", function (req, res) {
+        db.Adventure.create({
+            name: req.body.name,
+            description: req.body.description,
+            image: req.body.image,
+            longitude: req.body.longitude,
+            latitude: req.body.latitude,
+            AdventureCompanyId: req.body.AdventureCompanyId,
+        })
+            .then(function (data) {
+                console.log(data);
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    });
+};
