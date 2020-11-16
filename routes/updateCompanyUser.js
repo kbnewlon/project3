e4e56bde0a0981c4fe5812bdd794de351192c6dc
Andrew Crow <andrew@crow.me>
2020-11-16 13:35:04 -0800

    cleaned up formatting

--- routes/updateCompanyUser.js
+++ routes/updateCompanyUser.js
@@ -1,20 +1,22 @@
-const db = require('../models');
+const db = require("../models");
 
-//update adventure route 
+//update adventure route
 module.exports = (app) => {
     app.put("/company_user/:id", function (req, res) {
         db.Company_user.findOne({
-            where: { id: req.params.id }
-        }).then(function (userInfo) {
-            console.log(userInfo)
-            userInfo.update({
-                user_name: req.body.user_name,
-                password: req.body.password,
-                email: req.body.email
+            where: { id: req.params.id },
+        })
+            .then(function (userInfo) {
+                console.log(userInfo);
+                userInfo.update({
+                    user_name: req.body.user_name,
+                    password: req.body.password,
+                    email: req.body.email,
+                });
+                res.status(200).json(userInfo);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
             });
-            res.status(200).json(userInfo);
-        }).catch(err => {
-            res.status(500).json(err)
-        });
     });
 };
