1de789af454f8bd13cbf8e4b3ff9bcdb68dc342a
Andrew Crow <andrew@crow.me>
2020-11-16 13:34:30 -0800

    fixed update compnay user

--- routes/updateCompanyUser.js
+++ routes/updateCompanyUser.js
@@ -2,25 +2,19 @@ const db = require('../models');
 
 //update adventure route 
 module.exports = (app) => {
-    app.put('/company_user/:id', function (req, req) {
-        db.Adventure.update({
-            user_name: req.body.user_name,
-            password: req.body.password,
-            email: req.body.email
-        }, {
-            where: {
-                id: req.params.id
-            }
-        }).then(data => {
-            if (data[0] === 0) {
-                console.log(data);
-                res.status(200).json(data)
-            } else {
-                res.json(data)
-            }
+    app.put("/company_user/:id", function (req, res) {
+        db.Company_user.findOne({
+            where: { id: req.params.id }
+        }).then(function (userInfo) {
+            console.log(userInfo)
+            userInfo.update({
+                user_name: req.body.user_name,
+                password: req.body.password,
+                email: req.body.email
+            });
+            res.status(200).json(userInfo);
         }).catch(err => {
-            console.log(err)
-            res.status(500).json(err);
-        })
+            res.status(500).json(err)
+        });
     });
 };
