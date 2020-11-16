9ce5a5df0eedb2f2fddf8b3aea041e125b34871b
Andrew Crow <andrew@crow.me>
2020-11-16 00:17:56 -0800

    added fix for adventure route

--- routes/createAdventureCompany.js
+++ routes/createAdventureCompany.js
@@ -1,28 +1,28 @@
 const db = require("../models");
 
 //create new adventure company
-app.post('/adventure/company', function (req, res) {
-    db.Adventure_company.create({
-        Adventure_companyId: req.body.Adventure_companyId,
-        name: req.body.name,
-        address_1: req.body.address_1,
-        address_2: req.body.address_2,
-        city: req.body.city,
-        state: req.body.state,
-        zip_code: req.body.zip_code,
-        phone: req.body.phone,
-        email: req.body.email,
-        website: req.body.website,
-        description: req.body.description,
-        image: req.body.image,
-        taxId: req.body.taxId,
-        Company_userId: req.body.Company_userId,
-    }).then(function (data) {
-        console.log(data);
-        res.status(200).json(data);
-    }).catch(err => {
-        res.status(500).json(err)
-    })
-});
-
-module.exports = app;
+module.exports = (app) => {
+    app.post("/adventure/company", function (req, res) {
+        db.Adventure_company.create({
+            name: req.body.name,
+            address_1: req.body.address_1,
+            address_2: req.body.address_2,
+            city: req.body.city,
+            state: req.body.state,
+            zip_code: req.body.zip_code,
+            phone: req.body.phone,
+            email: req.body.email,
+            website: req.body.website,
+            description: req.body.description,
+            image: req.body.image,
+            CompanyUserId: req.body.CompanyUserId,
+        })
+            .then(function (data) {
+                console.log(data);
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    });
+};
