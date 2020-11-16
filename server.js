9671dd7fc72333e0575849ccdc06e1338b2044ab
Andrew Crow <andrew@crow.me>
2020-11-16 00:23:51 -0800

    added create adventure to server.js

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use(
 
 require("./routes/createCompanyUser")(app);
 require("./routes/createAdventureCompany")(app);
+require("./routes/createAdventure")(app);
 require("./routes/loginUser")(app);
 require("./routes/registerUser")(app);
 require("./routes/forgotPassword")(app);
