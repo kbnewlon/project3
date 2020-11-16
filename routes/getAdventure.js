0e1894c00b52915ed97e1deaf674c88808c32c96
Andrew Crow <andrew@crow.me>
2020-11-16 01:58:57 -0800

    fixed syntax to give all information on adventures

--- routes/getAdventure.js
+++ routes/getAdventure.js
@@ -1,30 +1,34 @@
 const db = require("../models");
 
 //get all adventures
-app.get('/adventures', function (req, res) {
-    db.Adventure.findAll({
-        include: [db.Tag, db.Rating, db.Adventure_company]
-    }).then(function (data) {
-        res.status(200).json(data);
-    }).catch(err => {
-        res.status(500).json(err)
+module.exports = (app) => {
+    app.get("/adventures", function (req, res) {
+        db.Adventure.findAll({
+            include: [db.Adventure_rating, db.Adventure_company, db.Tag],
+        })
+            .then(function (data) {
+                console.log(data);
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
     });
-});
 
-
-//get one adventure 
-app.get("/adventure/:id", function (req, res) {
-    db.Adventure.findOne({ 
-        where: { id: req.params.id }, 
-        include: [db.Tag, db.Rating, db.Adventure_company]
-     }).then(function (data) {
-        res.status(200).json(data);
-    }).catch(err => {
-        res.status(500).json(err)
+    //get one adventure
+    app.get("/adventure/:id", function (req, res) {
+        db.Adventure.findOne({
+            where: {
+                id: req.params.id,
+            },
+            include: [db.Adventure_rating, db.Adventure_company, db.Tag],
+        })
+            .then(function (data) {
+                console.log(data);
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
     });
-});
-
-
-
-
-module.exports = app;
+};
