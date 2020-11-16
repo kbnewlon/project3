9da267abaa0aa32e5079428c031c61d3c57c1923
Andrew Crow <andrew@crow.me>
2020-11-16 09:44:10 -0800

    chnaged git tag formatting

--- routes/getTag.js
+++ routes/getTag.js
@@ -2,24 +2,26 @@ const db = require("../models");
 
 module.exports = (app) => {
     //get all adventures
-    app.get('/tags', function (req, res) {
-        db.Tag.findAll({
-        }).then(function (data) {
-            res.status(200).json(data);
-        }).catch(err => {
-            res.status(500).json(err)
-        });
+    app.get("/tags", function (req, res) {
+        db.Tag.findAll({})
+            .then(function (data) {
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
     });
 
-
-    //get one adventure 
+    //get one adventure
     app.get("/tag/:id", function (req, res) {
         db.Tag.findOne({
-            where: { id: req.params.id }
-        }).then(function (data) {
-            res.status(200).json(data);
-        }).catch(err => {
-            res.status(500).json(err)
-        });
+            where: { id: req.params.id },
+        })
+            .then(function (data) {
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
     });
-}
+};
