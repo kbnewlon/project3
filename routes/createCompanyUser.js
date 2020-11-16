0ebf9637b2ac52a921cbcccf2274583b324c3379
Andrew Crow <andrew@crow.me>
2020-11-16 00:00:17 -0800

    fixed broken code in compnay user route

--- routes/createCompanyUser.js
+++ routes/createCompanyUser.js
@@ -1,17 +1,19 @@
 const db = require("../models");
 
 //create new adventure
-app.post('/company_user', function (req, res) {
-    db.Company_user.create({
-        user_name: req.body.user_name,
-        password: req.body.password,
-        email: req.body.email
-    }).then(function (data) {
-        console.log(data);
-        res.status(200).json(data);
-    }).catch(err => {
-        res.status(500).json(err)
-    })
-});
-
-module.exports = app;
+module.exports = (app) => {
+    app.post("/company_user", function (req, res) {
+        db.Company_user.create({
+            user_name: req.body.user_name,
+            password: req.body.password,
+            email: req.body.email,
+        })
+            .then(function (data) {
+                console.log(data);
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    });
+};
