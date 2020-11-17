a658c67b37666e9c43f394cc1be220275a84b695
Kayla Newlon <kbnewlon@hotmail.com>
2020-11-16 23:12:01 -0800

    added passport/passport.js

---
+++ config/passport/passport.js
@@ -0,0 +1,17 @@
+// const express = require("express");
+// const secureAPIRoute = require("./routes/secure-api-routes");
+
+
+// app.use(
+//     "/api",
+//     passport.authenticate("jwt", { session: false }),
+//     secureAPIRoute);
+
+
+// protected route
+// app.get('/protected', passport.authenticate('jwt', { session: false }), function(req, res) {
+//     res.json('Success! You can now see this without a token.');
+//   });
+
+
+// module.exports = app;
