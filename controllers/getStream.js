b783e9ee71cf5b37f42fe30f909fd26f7ca7f879
Kayla Newlon <kbnewlon@hotmail.com>
2020-11-17 11:35:40 -0800

    requiring express

--- controllers/getStream.js
+++ controllers/getStream.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+const express = require("express");
 const router = express.Router();
 const db = require("../models");
 const stream = require('getstream');
