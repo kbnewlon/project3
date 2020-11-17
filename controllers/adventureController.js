6ea60d7d6d0fbf85bb6e422c77a291ed56f86cc5
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2020-11-17 10:42:48 -0800

    updated adventureController.js

--- controllers/adventureController.js
+++ controllers/adventureController.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-//create new adventure
-
+// create new adventure
 router.post("/adventure", function (req, res) {
     db.Adventure.create({
         name: req.body.name,
@@ -23,7 +22,7 @@ router.post("/adventure", function (req, res) {
 });
 
 // delete adventure
-router.delete('/adventure/:id', function (req, res) {
+router.delete("/adventure/:id", function (req, res) {
     db.Adventure.destroy({
         where: {
             id: req.params.id
@@ -36,6 +35,59 @@ router.delete('/adventure/:id', function (req, res) {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+// get all adventures
+router.get("/adventures", function (req, res) {
+    db.Adventure.findAll({
+        include: [db.Adventure_rating, db.Adventure_company, db.Tag],
+    })
+        .then(function (data) {
+            console.log(data);
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+
+// get one adventure
+router.get("/adventure/:id", function (req, res) {
+    db.Adventure.findOne({
+        where: {
+            id: req.params.id,
+        },
+        include: [db.Adventure_rating, db.Adventure_company, db.Tag],
+    })
+        .then(function (data) {
+            console.log(data);
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+
+// update adventure
+router.put("/adventure/:id", function (req, res) {
+    db.Adventure.findOne({
+        where: { id: req.params.id },
+    })
+        .then(function (adventureInfo) {
+            console.log(adventureInfo);
+            adventureInfo.update({
+                name: req.body.name,
+                description: req.body.description,
+                image: req.body.image,
+                longitude: req.body.longitude,
+                latitude: req.body.latitude,
+                AdventureCompanyId: req.body.AdventureCompanyId
+            });
+            res.status(200).json(adventureInfo);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router
