3f21d4b12782f33a6b0881c998b187b52eda8a8f
Andrew Crow <andrew@crow.me>
2020-11-17 20:32:45 -0800

    added secure company signup route

--- controllers/signupCompanyUser.js
+++ controllers/signupCompanyUser.js
@@ -1,21 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const passport = require('passport');
 const db = require("../models");
 
 //create new company user
-router.post("/company_user", function (req, res) {
-    db.Company_user.create({
-        user_name: req.body.user_name,
-        password: req.body.password,
-        email: req.body.email,
-    })
-        .then(function (data) {
-            console.log(data);
-            res.status(200).json(data);
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        });
+router.post('/signup_company_user', (req, res, next) => {
+    passport.authenticate('registerCompany', (err, user, info) => {
+        if (err) {
+            console.error(err);
+        }
+        if (info !== undefined) {
+            console.error(info.message);
+            res.status(403).send(info.message);
+        } else {
+            req.logIn(user, error => {
+                console.log(user);
+                const data = {
+                    email: req.body.email,
+                    username: user.username,
+                };
+                console.log(data);
+                db.Company_user.findOne({
+                    where: {
+                        username: data.username,
+                    },
+                }).then(user => {
+                    console.log(user);
+                    user
+                        .update({
+                            email: data.email,
+                        })
+                        .then(() => {
+                            console.log('user created in db');
+                            res.status(200).send({ message: 'user created' });
+                        });
+                });
+            });
+        }
+    })(req, res, next);
 });
 
 module.exports = router
