9ec9a0762b4157274adf4e350092dce4a96089ec
Kayla Newlon <kbnewlon@hotmail.com>
2020-11-20 11:34:07 -0800

    added count route in in rating

--- controllers/adventureRatingController.js
+++ controllers/adventureRatingController.js
@@ -4,7 +4,7 @@ const db = require("../models");
 
 // create adventure rating
 router.post('/adventure_rating/:id', function (req, res) {
-    console.log("Hello World", req.user.dataValues.id)
+    console.log(req.user.dataValues.id)
     db.Adventure_rating.findAll({
         where: {
             AdventureId: req.params.id,
@@ -47,5 +47,24 @@ router.delete('/adventure_rating/:id/:userId', function (req, res) {
         res.status(500).json(err);
     });
 });
+
+
+//get like count
+router.get('/adventure_rating/:id', function (req, res){
+    db.Adventure_rating.count({
+        where: {
+            AdventureId: req.params.id
+        }
+    }).then(count => {
+        console.log(count)
+        res.status(200).json(count)
+
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+
 
 module.exports = router
